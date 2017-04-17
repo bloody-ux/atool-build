defc4f5ffddde2753a1ffb081856b5a39e41b929
绯一 <jaredlee.ljj@alipay.com>
2017-04-17 17:40:09 +0800

    feat: export deps plugins for further customizing by instaceof

--- src/webpack.js
+++ src/webpack.js
@@ -1,2 +1,7 @@
 import webpack from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import CaseSensitivePathsPlugin from 'case-sensitive-paths-webpack-plugin';
+
 export default webpack;
+
+export { ExtractTextPlugin, CaseSensitivePathsPlugin };
