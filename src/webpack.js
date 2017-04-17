bf5dccf21d4f0bbc60cc18e4b957051bc962e70b
绯一 <jaredlee.ljj@alipay.com>
2017-04-17 17:53:19 +0800

    Revert "feat: export deps plugins for further customizing by instaceof"
    This reverts commit defc4f5ffddde2753a1ffb081856b5a39e41b929.

--- src/webpack.js
+++ src/webpack.js
@@ -1,7 +1,2 @@
 import webpack from 'webpack';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import CaseSensitivePathsPlugin from 'case-sensitive-paths-webpack-plugin';
-
 export default webpack;
-
-export { ExtractTextPlugin, CaseSensitivePathsPlugin };
